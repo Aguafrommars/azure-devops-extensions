abff83059b444056b981f126c85b368920293146
aguacongas <aguacongas@gmail.com>
2021-12-08 17:45:20 +0100

    build: update make.cmd

--- pull-request-cz/make.cmd
+++ pull-request-cz/make.cmd
@@ -4,7 +4,7 @@ CALL terser scripts/main.js -o scripts/main.min.js -c -m
 CALL replace-in-file scripts/context.js scripts/context.min.js contextForm.html
 CALL replace-in-file scripts/main.js scripts/main.min.js main.html
 if "%APPVEYOR_REPO_BRANCH%" EQU "master" (
-    tfx extension create --override "{\"version\": \"%SemVer%.%GitVersion_BuildMetaData%\", \"name\": \"CommitiZen Pull Request (Preview)\"}" --overrides-file ..\override.json --output-path dist
+    tfx extension create --override "{""version"": ""%SemVer%.%GitVersion_BuildMetaData%"", ""name"": ""CommitiZen Pull Request (Preview)""}" --overrides-file ..\override.json --output-path dist
 ) else (
-    tfx extension create --override "{\"version\": \"%SemVer%.%GitVersion_BuildMetaData%\"}" --output-path dist
+    tfx extension create --override "{""version"": ""%SemVer%.%GitVersion_BuildMetaData%""}" --output-path dist
 )
